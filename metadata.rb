e637485b671faa67f439077a4c1497aee17f6cb1
Chris Henderson <chris.henderson@sage.com>
2014-10-09 14:24:54 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.5'
 
-include_recipe "nginx"
-include_recipe "ohai"
+depends "nginx"
+depends "ohai"
 
 %w{git nginx apache2 htpasswd}.each do |cb|
   depends cb
